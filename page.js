73ecfdad6159650e9095c92d031ec54b8e99ccc4
Aman190703 <155795703+Aman190703@users.noreply.github.com>
2024-07-04 18:22:00 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import GithubSearch from '/Components/GithubSearch'
+
+const page = () => {
+  return (
+    <div className='page'>
+      <GithubSearch />
+    </div>
+  )
+}
+
+export default page
+  
